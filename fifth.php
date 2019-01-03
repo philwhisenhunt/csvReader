8fd4844b903fcf84b05b5c31675bdebf00a679ac
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-03 16:42:45 -0500

    Seeing the order of events for while statement

--- fifth.php
+++ fifth.php
@@ -3,9 +3,10 @@
 $file = fopen("listFile.csv", 'r');
 
 while(($line = fgetcsv($file)) !== FALSE){
-    echo $line[0];
-
+    echo "\n " . $line[0];
+    echo "\n " . $line[1];
 }
 
+echo "\n ";
 fclose($file);
 
