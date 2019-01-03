443783c0157313b25c0b3639c020441450b7c49f
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-02 22:42:33 -0500

    Adding files

---
+++ csvReader.php
@@ -0,0 +1,41 @@
+<?php
+
+$file =fopen("listFile.csv", "r");
+$row = fgetcsv($file, 0, ",");
+
+foreach ($row as $cell){
+    if(is_bool($$cell)){
+    echo "boolean";
+}
+else{
+    echo "not boolean";
+}
+
+}
+echo $row[0];
+
+// function checker($file){
+
+
+//     //check for boolean
+ 
+
+// if(is_bool($col1)){
+//     echo "boolean";
+// }
+// else{
+//     echo "not boolean";
+// }
+
+// //check for numeric
+// if(numeric($col2)){
+//     echo "numeric";
+// }
+// else{
+//     echo "not numeric";
+// }
+
+
+// }
+
+// echo checker($file);
