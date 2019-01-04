cf108031c7e0761426ced3fc0de4f6d7db9eb109
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-04 14:17:21 -0500

    adding more after checking

--- sixth.php
+++ sixth.php
@@ -4,6 +4,17 @@ $file = fopen('listFile.csv', 'r');
 while(($line = fgetcsv($file)) !== FALSE){
 
 $lowered = strtolower($line[0]);
+    if($lowered == "true" || $lowered == "false"){
+        echo "\n $lowered is a boolean.";
+    }
+
+    else{
+        echo "/n $lowered is not a boolean.";
+    }
+
+    if(is_numeric($line[1])){
+        echo "\n " . $line[1] . " is numeric.";
+    }
 
 
 
