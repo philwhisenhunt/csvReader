0fc88a4ebbb95a0ccc542f5571ec46daf8d6d23e
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-01-04 13:37:47 -0500

    Adding functionality to fifth

--- fifth.php
+++ fifth.php
@@ -5,6 +5,11 @@ $file = fopen("listFile.csv", 'r');
 while(($line = fgetcsv($file)) !== FALSE){
     echo "\n " . $line[0];
     echo "\n " . $line[1];
+    $lowerNow = strtolower($line[0]);
+    
+    if($lowerNow === "true" || $lowerNow === "false"){
+        echo "$lowerNow is a boolean";
+    }
 }
 
 echo "\n ";
